8b3b09dbbaf4236286f67b46a82a7c3c2a45094c
Tabatha Bell <tabathambell@live.com>
2021-01-12 16:17:37 -0500

    changed ports

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 const notes = require('./data/notes.json');
@@ -22,6 +23,6 @@ app.get('/api/notes', (req, res) => {
     res.json(results);
 });
 
-app.listen(3001, () => {
-    console.log('API server now on port 3001!');
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
