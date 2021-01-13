1dd3524e8891039da853c980b0d3aadff41c2664
Tabatha Bell <tabathambell@live.com>
2021-01-13 00:22:55 -0500

    created validation and worked on POST and unique id's

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+const { v4: uuidv4, validate } = require('uuid');
 
 const notes = require('./data/notes.json');
 
@@ -15,12 +20,59 @@ function filterByQuery(query, notesArray) {
     return filteredResults;
 }
 
+function findById(id, notesArray) {
+    const result = notesArray.filter(notes => notes.id === id)[0];
+    return result;
+}
+
+function createNewNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './data/notes.json'),
+        JSON.stringify({ notes: notesArray }, null, 2)
+    );
+    return note;
+}
+
+function validateNote(note) {
+    if (!note.title || typeof note.title !== 'string') {
+        return false;
+    }
+    if (!note.text || typeof note.text !== 'string') {
+        return false;
+    }
+    if (!note.id || typeof note.id !== 'string') {
+        return false;
+    }
+    return true;
+}
+
 app.get('/api/notes', (req, res) => {
     let results = notes;
     if (req.query) {
         results = filterByQuery(req.query, results);
     }
     res.json(results);
+});
+
+app.get('/api/notes/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+  });
+
+app.post('/api/notes', (req, res) => {
+    req.body.id = uuidv4();
+    if (!validateNote(req.body)) {
+        res.status(400).send('The note is not properly formatted.')
+    } else {
+    const note = createNewNote(req.body, notes)
+    res.json(note);
+    }
 });
 
 app.listen(PORT, () => {
